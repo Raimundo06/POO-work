20513b1dadd2abd1130ea53cd41d774247f7c4ef
Raimundo06 <raimundo.sebastiao@aluno.ufca.edu.br>
2025-12-09 16:49:34 -0300

    Delete usuario.py

--- usuario.py
+++
@@ -1,7 +0,0 @@
-class Usuario:
-    def __init__(self, nome):
-        self.nome = nome
-        self.historico = []
-
-    def registrar_leitura(self, publicacao):
-        self.historico.append(publicacao)
