4934204afcec74eb4eccedf938ce6a983e973017
Raimundo06 <raimundo.sebastiao@aluno.ufca.edu.br>
2025-12-16 17:24:39 -0300

    Delete biblioteca_projeto directory

--- biblioteca_projeto/usuario.py
+++
@@ -1,7 +0,0 @@
-class Usuario:
-    def __init__(self, nome: str):
-        self.nome = nome
-        self.historico = []
-
-    def registrar_leitura(self, publicacao):
-        self.historico.append(publicacao)
